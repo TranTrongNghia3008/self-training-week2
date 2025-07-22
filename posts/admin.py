ba2783d365d0780efddc1386c199f102f3f82e7e
Phuoc Nguyen <phuoc.nguyenhoang@hdwebsoft.dev>
2025-07-22 16:35:30 +0700

    Add models, forms, and admin for Post and Comment

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
+from django.utils.http import urlencode
+from django import forms
+from .models import Post, Comment
 
-# Register your models here.
+@admin.action(description="Mark selected posts as Published")
+def publish_posts(modeladmin, request, queryset):
+    queryset.update(status='published')
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'created_at',         'short_description', 'view_comments_link')
+    list_filter = ('status', 'author', 'created_at')
+    search_fields = ('title', 'content')
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    actions = [publish_posts]
+
+    fieldsets = (
+        ("Post Content", {
+            "fields": ('title', 'content')
+        }),
+        ("Author & Status", {
+            "fields": ('author', 'status'),
+            "classes": ('collapse',),
+        }),
+    )
+
+    def short_description(self, obj):
+        description = obj.content[:10] + '...' if len(obj.content) > 10 else obj.content
+        return format_html('<span title="{}"><i>{}</i></span>', obj.content, description)   
+
+    short_description.short_description = 'Short Description'
+
+    def view_comments_link(self, obj):
+        count = obj.comments.count()
+        url = (
+            reverse('admin:posts_comment_changelist')
+            + '?'
+            + urlencode({'post__id': obj.id})
+        )
+        return format_html('<a href="{}">Comments ({})</a>', url, count)        
+    view_comments_link.short_description = 'View Comments'
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['title'].widget.attrs.update({'placeholder': 'Enter post title'})
+        form.base_fields['title'].label = 'Post Title'
+        return form
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'post', 'author', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('author', 'content')
+    ordering = ('-created_at',)
+
+    fieldsets = (
+        ("Comment Details", {
+            "fields": ('post', 'author', 'content')
+        }),
+    )
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post.title}'
